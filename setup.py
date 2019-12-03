edb6357f991b9a9dfc7767d34d5b2be56c69bfbb
InspireX96 <xumw1996@gmail.com>
2019-12-03 14:29:42 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='reacher_her_env',
+    version='0.0.1',
+    install_requires=['gym'],
+    author="Mingwei Xu",
+    author_email="xumw1996@gmail.com"
+)
