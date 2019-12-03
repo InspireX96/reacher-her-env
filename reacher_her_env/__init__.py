c96c456b7f49bcdd26c99f5c4416fae3981602e5
InspireX96 <xumw1996@gmail.com>
2019-12-03 14:27:50 -0800

    add env

---
+++ reacher_her_env/__init__.py
@@ -0,0 +1,10 @@
+"""Register new 2 Link robot arm"""
+from gym.envs.registration import register
+
+register(
+    id='ReacherHerEnv-v2',
+    entry_point='reacher_her_env.reacher_env_mod:ReacherHerEnv',
+    kwargs={'rand_init':True},
+    max_episode_steps=150,
+    reward_threshold=18.0,
+)
---
+++ reacher_her_env/reacher_env_mod.py
@@ -0,0 +1,45 @@
+"""
+Reacher HER Env
+"""
+
+import numpy as np
+from gym.envs.mujoco.reacher import ReacherEnv
+
+
+class ReacherHerEnv(ReacherEnv):
+    """
+    Reacher HER env
+
+    :param ReacherEnv: obj, original gym Reacher env
+    """
+
+    def __init__(self):
+        super().__init__(self)
+        # TODO: what is has_object in FetchEnv?
+        # NOTE: in FetchReach, has_object is False
+
+    def _get_obs(self):
+        """
+        Overrides _get_obs
+        """
+        # positions
+        # TODO: add noise?
+        achieved_goal = self.get_body_com("fingertip")
+        goal = self.get_body_com("target")
+
+        # original obs
+        theta = self.sim.data.qpos.flat[:2]
+
+        obs = np.concatenate([
+            np.cos(theta),
+            np.sin(theta),
+            self.sim.data.qpos.flat[2:],
+            self.sim.data.qvel.flat[:2],
+            self.get_body_com("fingertip") - self.get_body_com("target")
+        ])
+
+        return {
+            'observation': obs.copy(),
+            'achieved_goal': achieved_goal.copy(),
+            'desired_goal': goal.copy(),
+        }
